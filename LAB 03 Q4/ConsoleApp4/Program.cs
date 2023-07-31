a7d88291dc3c428aca37af94ab9b71e36e7f55fd
Anshath Ahamed <82915085+Anshath27@users.noreply.github.com>
2023-07-31 14:34:34 +0530

    Add files via upload

---
+++ LAB 03 Q4/ConsoleApp4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int startNumber;
+
+            
+            int endNumber;
+
+            
+            Console.WriteLine("Enter the starting number: ");
+            startNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the ending number: ");
+            endNumber = int.Parse(Console.ReadLine());
+
+            
+            int sum = 0;
+
+            
+            for (int i = startNumber; i <= endNumber; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    sum += i;
+                }
+            }
+
+            
+            Console.WriteLine("The sum of the odd numbers is: " + sum);
+
+            
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
