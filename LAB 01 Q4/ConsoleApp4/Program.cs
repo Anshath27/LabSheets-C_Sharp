f0c412045064fc9c83ac83313cae73448a12805a
Anshath Ahamed <82915085+Anshath27@users.noreply.github.com>
2023-07-31 14:30:25 +0530

    Add files via upload

---
+++ LAB 01 Q4/ConsoleApp4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int salary;
+            float taxRate;
+
+          
+            Console.WriteLine("Enter the salary: ");
+            salary = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the tax rate: ");
+            taxRate = float.Parse(Console.ReadLine());
+
+            
+            float tax = salary * taxRate;
+
+            
+            int salaryAfterTax = (int)(salary - tax);
+
+            
+            Console.WriteLine("The salary after the tax is: " + salaryAfterTax);
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
